ab8b54f8dd3cdfd41f90726b8435cf129224680c
JacobKnaack <jacobknck@gmail.com>
2024-01-24 12:06:08 -0800

    adds review

---
+++ class-03/review/about-me-/README.md
@@ -0,0 +1,28 @@
+# LAB - 02
+
+## Readme
+
+I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+
+ Author: Lisa
+
+201 Lab Links:
+
+*    http://127.0.0.1:5500/about-me-/index.html
+
+*   http://127.0.0.1:5500/reading-notes/102/reading02.md
+
+
+
+* [submission PR](http://xyz.com)
+* Any Links you used as reference
+
+### Lighthouse Accessibility Report Score # 100
+
+![description](img/lighthouse.png)
+* Provide a screenshot of your score after running a Lighthouse Accessibility report.
+
+### Reflections and Comments
+
+* Consider including the answers to your daily journal and submission questions here
+* This is also a good place to reflect on the tools and resources used and learned
---
+++ class-03/review/about-me-/js/app.js
@@ -0,0 +1,58 @@
+let question1 = prompt('Do I like plants?');
+let answer1 = 'yes';
+
+if (question1 === answer1) {
+    console.log ('your-correct');
+    alert('your-correct')
+} else {
+    console.log ('your-wrong');
+    alert('Your-wrong');
+}
+
+let question2 = prompt('Do I like rainy weather?');
+let answer2 = 'yes';
+
+if (question2 === answer2) {
+    console.log ('your-correct');
+    alert('your-correct');
+} else {
+    console.log ('your-wrong');
+    alert('your-wrong')
+}
+
+let question3 = prompt('Do I like mac and cheese?');
+let answer3 = 'yes';
+
+if (question3 === answer3) { 
+    console.log ('your-correct');
+    alert('your-correct')
+} else {
+    console.log ('your-wrong');
+    alert('your-wrong')
+}
+
+    
+let question4 = prompt('Do I like snow boarding?');
+let answer4 = 'yes';
+
+if (question4 === answer4) {
+    console.log ('your-correct');
+    alert('your-correct')
+} else {
+    console.log ('your-wrong');
+    alert('your-wrong')
+}
+
+
+let question5 = prompt('Do I like going to the movies');
+let answer5 = 'yes';
+
+if (question5 === answer5) {
+    console.log ('your-correct');
+    alert('your-correct')   
+} else {
+    console.log ('your-wrong');
+    alert('your-wrong')
+}
+
+
